f397a11491a4080acc043b17d29a6168faab92de
eflipe <felipecabaleiro@gmail.com>
2020-07-23 18:02:08 -0300

    03 - Users

--- practica_2/polls_proj/polls_proj/settings.py
+++ practica_2/polls_proj/polls_proj/settings.py
@@ -26,6 +26,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rest_framework',
+    'rest_framework.authtoken',
     'polls_app'
 ]
 
@@ -108,3 +109,13 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_URL = '/static/'
+
+REST_FRAMEWORK = {
+    'DEFAULT_AUTHENTICATION_CLASSES': (
+        'rest_framework.authentication.TokenAuthentication',
+        'rest_framework.authentication.SessionAuthentication',
+    ),
+    'DEFAULT_PERMISSION_CLASSES': (
+        'rest_framework.permissions.IsAuthenticated',
+    )
+}
